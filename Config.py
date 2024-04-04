39efca921f60e5e810be0b7eed6f9a924cda3833
Madhu472 <45090616+Madhu472@users.noreply.github.com>
2024-04-04 16:56:08 +0530

    Create config.py
    Adding config file

---
+++ Config.py
@@ -0,0 +1,10 @@
+key_value_dict = {
+    'api_type' : "azure",
+    'api_base' : r"https://XXXXX.openai.azure.com/",
+    'api_version' : '2023-05-15',
+    'api_key' : "XXXXXXXXXXXXX",
+    'comp_eng_dep_nm': 'XXXXXXXXXXX',# deployment name
+    'embed_eng_dep_nm': 'embedding', # deployment name for ada-model
+    'embedding_model':'text-embedding-ada-002',
+    'ai21_Key':"#######################################"
+} 
